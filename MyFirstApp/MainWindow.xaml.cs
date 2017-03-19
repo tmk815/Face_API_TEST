df7ad3106760edeb82a44b98802e440f35a351c8
tmk <monndenn15@gmail.com>
2017-03-19 14:32:01 +0900

    create

---
+++ MyFirstApp/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System.IO;
+
+namespace MyFirstApp
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IFaceServiceClient faceServiceClient = new FaceServiceClient("2d08a42789ad482c80fd1a5551449f36");
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openDlg = new Microsoft.Win32.OpenFileDialog();
+
+            openDlg.Filter = "JPEG Image(*.jpg)|*.jpg";
+            bool? result = openDlg.ShowDialog(this);
+
+            if (!(bool)result)
+            {
+                return;
+            }
+
+            string filePath = openDlg.FileName;
+
+            Uri fileUri = new Uri(filePath);
+            BitmapImage bitmapSource = new BitmapImage();
+
+            bitmapSource.BeginInit();
+            bitmapSource.CacheOption = BitmapCacheOption.None;
+            bitmapSource.UriSource = fileUri;
+            bitmapSource.EndInit();
+
+            FacePhoto.Source = bitmapSource;
+
+            Title = "Detecting...";
+            FaceRectangle[] faceRects = await UploadAndDetectFaces(filePath);
+            string something = await etc(filePath);
+            var member = new tb
+            {
+                Age_Face = something
+            };
+            this.DataContext = member;
+            Title = String.Format("Detection Finished. {0} face(s) detected", faceRects.Length);
+
+            if (faceRects.Length>0)
+{
+                DrawingVisual visual = new DrawingVisual();
+                DrawingContext drawingContext = visual.RenderOpen();
+                drawingContext.DrawImage(bitmapSource,
+                    new Rect(0, 0, bitmapSource.Width, bitmapSource.Height));
+                double dpi = bitmapSource.DpiX;
+                double resizeFactor = 96 / dpi;
+
+                foreach (var faceRect in faceRects)
+                {
+                    drawingContext.DrawRectangle(
+                        Brushes.Transparent,
+                        new Pen(Brushes.Blue, 2),
+                        new Rect(
+                            faceRect.Left * resizeFactor,
+                            faceRect.Top * resizeFactor,
+                            faceRect.Width * resizeFactor,
+                            faceRect.Height * resizeFactor
+                            )
+                    );
+                }
+
+                drawingContext.Close();
+                RenderTargetBitmap faceWithRectBitmap = new RenderTargetBitmap((int)(bitmapSource.PixelWidth * resizeFactor),(int)(bitmapSource.PixelHeight * resizeFactor),96,96,PixelFormats.Pbgra32);
+
+                faceWithRectBitmap.Render(visual);
+                FacePhoto.Source = faceWithRectBitmap;
+            }
+        }
+
+        private async Task<FaceRectangle[]> UploadAndDetectFaces(string imageFilePath)
+        {
+            try
+            {
+                using (Stream imageFileStream = File.OpenRead(imageFilePath))
+                {
+                    var faces = await faceServiceClient.DetectAsync(imageFileStream, returnFaceAttributes:new FaceAttributeType[] { FaceAttributeType.Gender, FaceAttributeType.Age, FaceAttributeType.FacialHair, FaceAttributeType.HeadPose, FaceAttributeType.Smile, FaceAttributeType.Glasses }
+);
+                    var faceRects = faces.Select(face =>face.FaceRectangle);
+                    return faceRects.ToArray();
+                }
+            }
+            catch (Exception)
+            {
+                return new FaceRectangle[0];
+            }
+        }
+
+        private async Task<string> etc(string imageFilePath)
+        {
+            try
+            {
+                using (Stream imageFileStream = File.OpenRead(imageFilePath))
+                {
+                    var faces = await faceServiceClient.DetectAsync(imageFileStream, returnFaceAttributes: new FaceAttributeType[] { FaceAttributeType.Gender, FaceAttributeType.Age, FaceAttributeType.FacialHair, FaceAttributeType.HeadPose, FaceAttributeType.Smile, FaceAttributeType.Glasses });
+                    string age="";
+                    string gender="";
+                    string smile = "";
+                    for (int i=0; i < faces.Length; i++)
+                    {
+                        age = smile + faces[i].FaceAttributes.Age.ToString() + "：";
+                        gender = age + faces[i].FaceAttributes.Gender.ToString() + "：";
+                        smile = gender + faces[i].FaceAttributes.Smile.ToString() + "\n";
+                    }
+                    return smile;
+                }
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+    }
+}
